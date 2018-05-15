b9f99067ef550f3aa1ece6ddd30915d993040ffa
seungprk@gmail.com <seungprk@gmail.com>
2018-05-14 22:22:59 -0700

    Swap out express for fastify

--- src/server.js
+++ src/server.js
@@ -1,13 +1,8 @@
 require('newrelic');
-const express = require('express');
-const morgan = require('morgan');
 const serverCore = require('./serverCore.js');
 
 const port = process.env.PORT || 3000;
 const { app } = serverCore;
-
-app.use(morgan('dev'));
-app.use(express.static('dist'));
 
 serverCore.startFetches()
   .then(() => {
